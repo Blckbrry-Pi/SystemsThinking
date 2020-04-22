3816f3f7a97eefd2d04e6e76a3cf41939c014f9e
Blckbrry-Pi <54462713+Blckbrry-Pi@users.noreply.github.com>
2020-04-22 15:04:18 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,37 @@
+let level0;
+let level1;
+let level2;
+let level3;
+let level4;
+let level5;
+let level6;
+let level7;8
+function preload(){
+  x = loadJSON('https://rectangularshowyoffices--kakklalamana.repl.co/level0.json')
+}
+
+function setup() {
+  createCanvas(innerWidth, innerHeight, WEBGL);
+}
+
+function draw() {
+  createCanvas(innerWidth, innerHeight, WEBGL);
+  drawLevel();
+  background(220);
+}
+
+function drawLevel(levelData) {
+
+}
+
+function drawBasicPlanes(planeData) {
+
+}
+
+function drawCeilingsAndFloors(levelData) {
+
+}
+
+function drawPlane(objectIn) {
+
+}
